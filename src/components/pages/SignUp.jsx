655e70b74fff5a7fceed1e863d63fb89abea1c6f
patel muhammad <148962037+patel192@users.noreply.github.com>
2025-09-17 02:03:56 +0530

    navigation

--- src/components/pages/SignUp.jsx
+++ src/components/pages/SignUp.jsx
@@ -28,7 +28,7 @@ export const SignUp = () => {
       localStorage.setItem("user", JSON.stringify(res.data.user));
 
       alert("Account created successfully!");
-      navigate("/dashboard"); // redirect where you want
+      navigate("/login"); // redirect where you want
     } catch (err) {
       console.error(err);
       alert(err.response?.data?.message || "Signup failed");
