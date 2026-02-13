0fdb18b378fb869763081c44195492f8a603f175
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-02-13 10:51:18 +0530

    Add Frontend CI Setup

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],server:{
-    host:true
+  plugins: [tailwindcss(), react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.js",
+    globals: true,
+  },
+  server: {
+    host: true,
   },
   build: {
     sourcemap: false,
-  }
-})
+  },
+});
