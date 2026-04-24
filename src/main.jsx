f342ace2ad942cf7d40e8f551fc465a96f4339e5
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-04-24 12:07:20 +0530

    feat: integrate redux-persist for state persistence in Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import "./theme.css";
-import {store} from "./redux/store.js";
+import {store,persistor} from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { SettingsProvider } from "./context/SettingsContext.jsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
   <AuthProvider>
     <SettingsProvider>
       <BrowserRouter>
@@ -16,5 +18,6 @@ createRoot(document.getElementById("root")).render(
       </BrowserRouter>
     </SettingsProvider>
   </AuthProvider>,
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./features/auth/authSlice";
+import { persistStore,persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+const persistConfig = {
+  key:"root",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(
+  persistConfig,
+  authReducer
+)
 export const store = configureStore({
   reducer:{
-    auth:authReducer,
+    auth:persistedAuthReducer,
   }
 });
+
+export const persistor = persistStore(store);
