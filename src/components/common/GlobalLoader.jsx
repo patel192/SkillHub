9deb7291658e4de737656b3d01e0387ee6217b42
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-04-24 21:13:55 +0530

    fix: add Redux state logging to GlobalLoader component

--- src/components/common/GlobalLoader.jsx
+++ src/components/common/GlobalLoader.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
+import React from "react";
+import { useSelector } from "react-redux";
 export const GlobalLoader = () => {
-  return (
-    <div>Loader Mounted</div>
-  )
-}
+  const state = useSelector((state) => state);
+
+  console.log("Redux state:", state);
+  return <div>Loader Mounted</div>;
+};
