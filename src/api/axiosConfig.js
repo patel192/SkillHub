990ba81d9b8ed05ad2e1e1fb8ff6ddde5dcbdc0a
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-04-24 21:36:18 +0530

    fix: improve code readability in axiosConfig.js by formatting and adding comments

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,11 @@
 import axios from "axios";
-import {logout} from "../redux/features/auth/authSlice";
-import {store} from "../redux/store";
-import { startLoading,stopLoading } from "../redux/features/ui/uiSlice";
-// Create axios instance with base URL
+import { logout } from "../redux/features/auth/authSlice";
+import { store } from "../redux/store";
+import {
+  startLoading,
+  stopLoading,
+} from "../redux/features/ui/uiSlice";
+
 const apiClient = axios.create({
   baseURL: "https://skillhub-backend-gs3t.onrender.com",
   timeout: 10000,
@@ -11,31 +14,44 @@ const apiClient = axios.create({
   },
 });
 
-// Request interceptor - automatically add token to every request
+//
+// REQUEST
+//
+
 apiClient.interceptors.request.use(
   (config) => {
     store.dispatch(startLoading());
+
     const token = localStorage.getItem("token");
+
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization =
+        `Bearer ${token}`;
     }
+
     return config;
   },
+
   (error) => {
+    store.dispatch(stopLoading());
     return Promise.reject(error);
   }
 );
 
-// Response interceptor - handle errors globally
+//
+// RESPONSE
+//
+
 apiClient.interceptors.response.use(
   (response) => {
-   store.dispatch(stopLoading())
-    return response
+    store.dispatch(stopLoading());
+    return response;
   },
 
   (error) => {
+    store.dispatch(stopLoading());
+
     if (error.response?.status === 401) {
-      store.dispatch(stopLoading());
       console.log("Session expired — logging out");
 
       store.dispatch(logout());
