b23361bd4d0a5e67739edb184c25babd5687c05f
patel muhammad <148962037+patel192@users.noreply.github.com>
2026-04-24 21:06:19 +0530

    fix: simplify GlobalLoader component by removing unused loading state and styles

--- src/components/common/GlobalLoader.jsx
+++ src/components/common/GlobalLoader.jsx
@@ -1,23 +1,7 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React from 'react'
+
 export const GlobalLoader = () => {
-  const loading = useSelector((state) => state.ui.loading);
-  if (!loading) return null;
   return (
-    <div
-    style={{
-        position:"fixed",
-        top:0,
-        left:0,
-        width:"100%",
-        height:"100%",
-        background:"rgba(0,0,0,0.4)",
-        display:"flex",
-        justifyContent:"center",
-        zIndex:9999,
-    }}
-    >
-      <div>GlobalLoader</div>
-    </div>
-  );
-};
+    <div>Loader Mounted</div>
+  )
+}
